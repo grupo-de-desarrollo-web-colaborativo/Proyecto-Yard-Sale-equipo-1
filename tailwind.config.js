3f0d1f56860a37142118ffbbb285644e607d18f4
lexandfr <alexfloresrayme@gmail.com>
2023-01-22 08:43:33 -0500

    config tailwind style

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "xtal-green": {
+        DEFAULT: "#ACD9B2",
+        white: "#FFFFFF",
+        black: "#000000",
+        dark: "#232830",
+        vlpink: "#C7C7C7",
+        tifield: "#F7F7F7",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
