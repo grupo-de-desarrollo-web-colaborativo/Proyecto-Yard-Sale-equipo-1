2b551443538fedfc72670d3b550f9fd03954666f
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2023-02-08 12:41:50 -0400

    section products tailwind and details of products with js

---
+++ src/list-products.js
@@ -0,0 +1,82 @@
+products = [
+  {
+    id: 1,
+    price: 120.0,
+    name: 'Round Shelf',
+    image: '../assets/images/round_shelf.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+  {
+    id: 2,
+    price: 130.0,
+    name: 'Retro refrigerator',
+    image: '../assets/images/fridge copy.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+  {
+    id: 3,
+    price: 125.0,
+    name: 'Round Shelf',
+    image: '../assets/images/round_shelf.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+  {
+    id: 4,
+    price: 122.0,
+    name: 'Retro refrigerator',
+    image: '../assets/images/fridge copy.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+  {
+    id: 5,
+    price: 125.0,
+    name: 'Round Shelf',
+    image: '../assets/images/round_shelf.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+  {
+    id: 6,
+    price: 123.0,
+    name: 'Retro refrigerator',
+    image: '../assets/images/fridge copy.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+  {
+    id: 7,
+    price: 128.0,
+    name: 'Round Shelf',
+    image: '../assets/images/round_shelf.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+  {
+    id: 8,
+    price: 124.0,
+    name: 'Retro refrigerator',
+    image: '../assets/images/fridge copy.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+  {
+    id: 9,
+    price: 125.0,
+    name: 'Round Shelf',
+    image: '../assets/images/round_shelf.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+  {
+    id: 10,
+    price: 126.0,
+    name: 'Retro refrigerator',
+    image: '../assets/images/fridge copy.jpg',
+    description:
+      'With its functional and practical interior, this refrigerator also fulfills a decorative function, adding personality and a retro-vintage aesthetic to your kitchen or workplace.',
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+  // content: ['./*.html'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
-        clrPrimary: '#acd9b2',
-        clrPrimary10: '#e7e7e7',
-        clrSecondary: '#ffffff',
-        clrText: '#797979',
-        clrText10: '#c7c7c7',
+        mossGreen: '#acd9b2',
+        mercury: '#e7e7e7',
+        white: '#ffffff',
+        Boulder: '#797979',
+        Silver: '#c7c7c7',
       },
+      // width: {
+      //   88: '23.4375rem',
+      // },
+      // height: {
+      //   88: '23.25rem',
+      // },
     },
   },
   plugins: [],
