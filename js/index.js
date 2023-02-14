20aa5f60ba7b75ec6e22eac8ea0c5dc1ee80d093
Edwin Miguel <edwinmiguelorellana@gmail.com>
2023-02-14 18:37:42 -0500

    primera parte del login y vista de error por carlos

--- js/index.js
+++ js/index.js
@@ -1,14 +1,8 @@
+// Interacción para ocultar la vista mobil del Login agregando una clase 'hidden' al elemento contenedor del formulario del login.
 const close = document.querySelector('#iconClose');
 const mobileLogin = document.querySelector('#formContainer');
 const header = document.querySelector('#header');
 
 close.addEventListener('click', () => {
   mobileLogin.classList.add('hidden');
-  header.classList.add('active-header');
 })
-
-const email = document.querySelector('#inputEmail');
-const password = document.querySelector('#inputPassword');
-
-
-
