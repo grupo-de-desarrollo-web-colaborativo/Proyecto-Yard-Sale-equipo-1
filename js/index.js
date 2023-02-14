7aa4be9b7df17d416913ee086eda687247dae3a3
Edwin Miguel <edwinmiguelorellana@gmail.com>
2023-02-14 18:13:48 -0500

    preparación de los archivos listos para push a la rama development 1

--- js/index.js
+++ js/index.js
@@ -11,43 +11,4 @@ const email = document.querySelector('#inputEmail');
 const password = document.querySelector('#inputPassword');
 
 
-// class Account {
-//   constructor({
-//     email, 
-//     password,
-//     nickname
-//   }) {
-//     this.email = email;
-//     this.password = password;
-//     this.nickname = nickname;
-//   }
-
-// }
-
-// let admin = new Account({
-//   email:  "camilayokoo@gmail.com",
-//   nickname: "yokoo",
-//   password: "lseflsjfe"
-// });
-
-// const cuentas = [];
-
-// const logIn = document.querySelector('#logInButton');
-
-// logIn.addEventListener('click', () => {
-//   if (email.value && password.value) {
-//     localStorage.setItem('email', email.value);
-//     localStorage.setItem('password', password.value);
-    
-//     const account = {
-//       email: localStorage.getItem('email'),
-//       password: localStorage.getItem('password')
-//     }
-//     cuentas.push(account);
-//     console.log(cuentas);
-//   } else {
-//     console.log('escribe el email y contraseña');
-//   }
-// })
-
 
