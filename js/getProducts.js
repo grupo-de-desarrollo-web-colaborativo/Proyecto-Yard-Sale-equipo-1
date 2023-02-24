c2cd4d4acb34af2c320dadd3791a92fd7679f3a4
Edwin Miguel <edwinmiguelorellana@gmail.com>
2023-02-23 23:00:17 -0500

    añado los detalles del producto

--- js/getProducts.js
+++ js/getProducts.js
@@ -1,6 +1,10 @@
 const API = "https://api.escuelajs.co/api/v1/products";
 const productsList = null || document.querySelector('#productsSection');
+let products; 
 let productsArray = [];
+let iconsAdd;
+let productsImages;
+let cartProducts = [];
 
 async function fetchData(urlApi) {
   const response = await fetch(urlApi);
@@ -9,14 +13,13 @@ async function fetchData(urlApi) {
 }
 
 
-const printProducts = async function () {
+async function printProducts() {
   try {
-    let products = await fetchData(API);
-    
+    products = await fetchData(API);
     let card = `
     ${products.map(product => `
-      <div class="card" id="product-${product.id}">
-        <img src="${product.images[0]}" alt="${product.description}" class="card__images productImage" id="${product.id}" />
+      <div class="card">
+        <img src="${product.images[0]}" alt="${product.title}" class="card__images productImage" id="${product.id}" />
           <div class="card__content">
             <div class="card__description">
               <p class="card__price">$ ${product.price}</p>
@@ -28,33 +31,48 @@ const printProducts = async function () {
     `).slice(0,5).join('')}
     `; 
     productsList.innerHTML = card;
-    products = products.slice(0,5);
-    productsArray.push(products);
-    showDetails ();
-    addToCart();
   } catch(error) {
     console.log(error);
   }
+
+  productsArray.push(products.slice(0,5));
+  productsArray = productsArray.flat();
+  console.log(productsArray);
+
+  //listener del click a la imagen de cada producto
+  productsImages = document.querySelectorAll('.productImage');
+  productsImages = [...productsImages];
+  productsImages.forEach( image => 
+    image.addEventListener('click', (event) =>{
+      showDetails(event);
+    })
+  );
+
+  //listener del icono add to cart
+  iconsAdd = document.querySelectorAll('.iconAdd');
+  iconsAdd = [...iconsAdd];
+  iconsAdd.forEach(element => {
+    element.addEventListener('click', (event) => {
+    addToCart(event);
+    })
+  });
 };
 
 printProducts();
+
 
 console.log(productsArray);
 
 const detailsContainer = document.querySelector('#DetailsContainer');
 const detailsCartButton = document.querySelector('#detailsCartButton');
+
 //mostrar detalles del producto 
-function showDetails() {
-  const productsImages = document.querySelectorAll('.productImage');
-  
-  productsImages.forEach( image => {
-    image.addEventListener('click', (event) =>{
-      let idImage = event.target.id;
-      let findProduct = productsArray[0].find(product => product.id == idImage);
-      console.log(idImage);
-      console.log(findProduct);
-      
-      let detailsContent = `
+function showDetails(e) {
+  let imageId = parseInt(e.target.id);
+  let findProduct = productsArray.find((product) => product.id == imageId);
+  console.log(findProduct);
+
+  let detailsContent = `
         <div class="product-details__close-icon-container" id="detailsClose">
           <img src="./assets/icons/icon_close.png" alt="" class="product-details__close-icon" />
         </div>
@@ -70,38 +88,37 @@ function showDetails() {
           </div>
        </div>
       `;
-      detailsContainer.innerHTML = detailsContent;
-      detailsContainer.classList.remove('hidden');
-      
-      //cerrando la ventana de detalles 
-      const detailsClose = document.getElementById('detailsClose');
-      detailsClose.addEventListener('click', ()=> detailsContainer.classList.add('hidden'));
-  })
-  });
+  detailsContainer.innerHTML = detailsContent;
+  detailsContainer.classList.remove("hidden");
+
+  //cerrando la ventana de detalles
+  const detailsClose = document.getElementById("detailsClose");
+  detailsClose.addEventListener("click", () =>
+    detailsContainer.classList.add("hidden")
+  );
 }
 
 //agrega el producto al shopping cart
 const cart = document.querySelector('#cardsContainer');
 const pTotal = document.querySelector('#totalPrice');
 const productsCount = document.querySelector('.carrito_compra_punto');
-let iconAdd;
-let cartProducts = [];
-let total = [];
-let deleteIcon;
-function addToCart() {
-  iconAdd = document.querySelectorAll('.iconAdd');
-  iconAdd = [...iconAdd];
-  iconAdd.forEach(element => {
-    element.addEventListener('click', (event) => { 
-    let currentId = parseInt(event.target.id);
-    console.log(currentId);
-    
-    let currentProduct = productsArray[0].find(product => product.id == currentId);
-    console.log(currentProduct);
 
-    cartProducts.push(currentProduct);
 
-    cart.innerHTML += `
+let pricesArray = [];
+let deleteIcons;
+let eventAdd = [];
+function addToCart(e) {
+  eventAdd.push(e);
+  //cambiando el icono de add to cart a added to cart
+  e.target.src = "./assets/icons/bt_added_to_cart.svg";
+
+  let currentId = parseInt(e.target.id);
+  let currentProduct = productsArray.find((product) => product.id == currentId);
+  console.log(currentProduct);
+
+  cartProducts.push(currentProduct);
+
+  cart.innerHTML += `
       <div class="product-card">
         <div class="product-card__left-container">
           <img class="product-card__img" src="${currentProduct.images[0]}" alt="${currentProduct.title}" />
@@ -113,54 +130,54 @@ function addToCart() {
           <span class="close-icon delete-icon" id="${currentProduct.id}"></span>
         </div>
       </div>
-    `
-    countProducts();
-    
-    total.push(currentProduct.price);
-    
-    calculateTotalPrice();
-    //cambiando el icono de add to cart a added to cart
-      let currentCart = document.getElementById(`${currentProduct.id}`);
-      currentCart.src = "./assets/icons/bt_added_to_cart.svg";
-      
+  `;
+  countProducts();
+  pricesArray.push(currentProduct.price);
+  calculateTotalPrice();
 
-    //eleminando el producto del shopping cart
-    function deleteFromCart() {
-      deleteIcon = document.querySelectorAll('.delete-icon');
-    deleteIcon = [...deleteIcon];
-    deleteIcon.forEach(element => {
-      element.addEventListener('click', (event) => {
-        let currentItem = event.target.id;
-        let findItem = productsArray[0].find(product => product.id == currentItem);
-        console.log(findItem);
-          // Eliminamos el producto de la variable cartProducts
-        let index = cartProducts.indexOf(findItem);
-        cartProducts.splice(index, 1);
-
-        // Eliminamos el producto del carrito
-        let productCard = event.target.closest('.product-card');
-        productCard.remove();
-
-         // Actualizamos el precio total
-         countProducts();
-         total.splice(index, 1);
-         calculateTotalPrice();
-
-          //cambiando el icono de added to cart a add to cart
-        let currentCart = document.getElementById(`${currentItem}`);
-        currentCart.src = "./assets/icons/bt_add_to_cart.svg";
-      })
-    });
-    }
-    deleteFromCart();
-
-    });
+  deleteIcons = document.querySelectorAll(".delete-icon");
+  deleteIcons = [...deleteIcons];
+  deleteIcons.forEach((item) => {
+    item.addEventListener("click", (event) =>
+      deleteFromCart(event))
   });
 }
 
-//suamr precios de todos los productos agregados al shopping cart
+//eleminando el producto del shopping cart
+function deleteFromCart(e) {
+  
+  let currentItem = parseInt(e.target.id);
+  let findItem = productsArray.find((product) => product.id == currentItem);
+  
+  // Eliminamos el producto de la variable cartProducts
+  let index = cartProducts.indexOf(findItem);
+  cartProducts.splice(index, 1);
+  
+  // Eliminamos el producto del carrito
+  let productCard = e.target.closest(".product-card");
+  productCard.remove();
+  
+  // Actualizamos el precio total
+  countProducts();
+  pricesArray.splice(index, 1);
+  calculateTotalPrice();
+
+  //cambiando el icono de added to cart a add to cart
+  eventAdd.forEach((item) => {
+    if (item.target.id ==  e.target.id) {
+
+      item.target.src = "./assets/icons/bt_add_to_cart.svg";
+      // if (!findEqual) {
+      // } else {
+      //   console.log("aun esta el producto en el carrito!");
+      // }
+    } 
+  });
+}
+
+//sumar precios de todos los productos agregados al shopping cart
 function calculateTotalPrice() {
-  let totalPrice = total.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  let totalPrice = pricesArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   pTotal.textContent = `$ ${totalPrice.toFixed(2)}`;
 }
 
